f440447118e65fc0292b0f9053a5ce342e9fe322
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-08-13 15:01:32 +0300

    Emoji keybord activated by the help of rn emoji keybord library

--- src/ChatScreen/MessageInput.js
+++ src/ChatScreen/MessageInput.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, StyleSheet,Dimensions } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, TextInput, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import EmojiKeyboard from 'rn-emoji-keyboard'; // Import EmojiKeyboard
 
 const { width } = Dimensions.get('window');
 
-
 const MessageInput = ({ onSendMessage }) => {
   const [message, setMessage] = useState('');
+  const [showEmojiKeyboard, setShowEmojiKeyboard] = useState(false); // State to toggle emoji keyboard
 
   const handleTextChange = (text) => {
     setMessage(text);
@@ -17,9 +18,20 @@ const MessageInput = ({ onSendMessage }) => {
     setMessage(''); // Clear the TextInput after sending the message
   };
 
+  const handleEmoticonPress = () => {
+    setShowEmojiKeyboard(!showEmojiKeyboard); // Toggle emoji keyboard visibility
+  };
+
+  const handleEmojiSelect = (emoji) => {
+    setMessage(prevMessage => prevMessage + emoji); // Add selected emoji to the message
+    setShowEmojiKeyboard(false); // Hide emoji keyboard after selecting an emoji
+  };
+
   return (
     <View style={styles.container}>
-      <Icon name="insert-emoticon" size={30} color="gray" style={styles.icon} />
+      <TouchableOpacity onPress={handleEmoticonPress}>
+        <Icon name="insert-emoticon" size={30} color="gray" style={styles.icon} />
+      </TouchableOpacity>
       <TextInput
         style={styles.input}
         placeholder="Mesaj"
@@ -42,28 +54,34 @@ const MessageInput = ({ onSendMessage }) => {
           style={styles.icon}
         />
       </TouchableOpacity>
+      {showEmojiKeyboard && (
+        <EmojiKeyboard
+          onEmojiSelected={handleEmojiSelect}
+          onBackspace={() => {}}
+        />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: width * 0.03, // Adjust padding based on screen width
-      backgroundColor: '#fff',
-      borderRadius: 25,
-      borderWidth: 1,
-      borderColor: '#ddd',
-    },
-    icon: {
-      marginHorizontal: width * 0.02, // Adjust margin based on screen width
-    },
-    input: {
-      flex: 1,
-      paddingHorizontal: width * 0.03, // Adjust padding based on screen width
-      fontSize: 16,
-    },
-  });
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: width * 0.03, // Adjust padding based on screen width
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  icon: {
+    marginHorizontal: width * 0.02, // Adjust margin based on screen width
+  },
+  input: {
+    flex: 1,
+    paddingHorizontal: width * 0.03, // Adjust padding based on screen width
+    fontSize: 16,
+  },
+});
 
 export default MessageInput;
